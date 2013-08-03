56bff733c8ea5ab7374acfaa53b880944bd3a301
Yomguithereal <guillaumeplique@msn.com>
2013-08-03 16:16:49 +0200

    Adding python 2.7 to classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='colifrapy',
       version='0.3.0',
-      description='Command Line Framework',
+      description='Command Line Framework for Python',
       url='https://github.com/Yomguithereal/colifrapy.git',
       long_description="Colifrapy is a Command Line Framework for Python. Its aim is to provide several tools to build robust and structured command line tools very easily.",
       author='Yomguithereal',
@@ -26,6 +26,7 @@ setup(name='colifrapy',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
             'Topic :: Software Development :: Code Generators',
             'Topic :: Software Development :: Libraries :: Application Frameworks'
       ]
