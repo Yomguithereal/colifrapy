bc2b4b496171d57aefe772481287836bc46d7eb0
Yomguithereal <guillaumeplique@gmail.com>
2013-12-15 18:21:21 +0100

    Finalized removing custom exceptions

--- colifrapy/__init__.py
+++ colifrapy/__init__.py
@@ -4,6 +4,5 @@ from .logger import Logger
 from .commander import Commander
 from .model import Model
 from .settings import Settings
-from .exception import DataException
 from .cacher import LineCacher
 from .cacher import YAMLCacher
